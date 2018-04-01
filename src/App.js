b29e75d26304fa271a01950364367c74a1dcbdda
Kunal Ganglani <kunalganglani@gmail.com>
2018-04-01 19:14:17 +0530

    implemented react bootstrap component well alert

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Well, Alert } from 'react-bootstrap';
+
 
 class App extends Component {
   render() {
@@ -13,6 +15,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Well>I am Mandvi Singh</Well>
+        <Alert bsStyle="warning">
+          <strong>Holy guacamole!</strong> Best check yo self, you're not looking too
+          good.
+        </Alert>;
       </div>
     );
   }
